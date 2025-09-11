31afdaa525de331ba9515c30058b4f94f9676e7d
bettariraju741 <bettariraju741@gmail.com>
2025-09-11 14:37:39 +0530

    added count folder

---
+++ count/variable.tf
@@ -0,0 +1,14 @@
+variable "common_tags" {
+    type = map
+    default = {
+        Environment = "Dev"
+        Project = "Expense"
+        Terraform = "true"
+    }
+}
+
+variable "instance_names" {
+    type = list(string)
+    default = [ "mysql","backend","frnotend" ]
+  
+}
