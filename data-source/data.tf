ba50330b39d3ee4c62ba453859f0e3864a142a31
bettariraju741 <bettariraju741@gmail.com>
2025-09-16 11:03:55 +0530

    added data-source

---
+++ data-source/data.tf
@@ -0,0 +1,19 @@
+data "aws_ami" "ami_info" {
+    most_recent = true
+    owners = ["277707113105"]
+
+    filter {
+      name = "name"
+      values = ["RHEL-9-DevOps-Practice"]
+    }
+    filter {
+      name = "root-device-type"
+      values = ["ebs"]
+    }
+    filter {
+      name = "virtualization-type"
+      values = ["hvm"]
+    }
+  
+}
+
